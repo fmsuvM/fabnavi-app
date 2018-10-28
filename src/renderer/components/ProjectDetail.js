2f1ca8c9e8e2cc778a5bf6d54b9817b9c499b38e
fmsuvM <k222.tkd.fms@gmail.com>
2018-10-28 14:40:08 +0900

    lint fix

--- src/renderer/components/ProjectDetail.js
+++ src/renderer/components/ProjectDetail.js
@@ -11,113 +11,113 @@ import CaptionList from './CaptionList';
 import { sanitizeProject } from '../utils/projectUtils';
 
 import {
-    StyledDetailFrame,
-    ProjectTitle,
-    ContentsFrame,
-    DescriptionFrame,
-    StyledHead,
-    StyledDescription,
-    StatusFrame,
-    StatusText,
-    PrivateNotation,
-    TagFrame,
-    TagHeader,
-    TagField,
-    StyledTagName
+  StyledDetailFrame,
+  ProjectTitle,
+  ContentsFrame,
+  DescriptionFrame,
+  StyledHead,
+  StyledDescription,
+  StatusFrame,
+  StatusText,
+  PrivateNotation,
+  TagFrame,
+  TagHeader,
+  TagField,
+  StyledTagName
 } from '../stylesheets/application/ProjectShow/StyledProjectDetail';
 
 const debug = Debug('fabnavi:jsx:ProjectDetail');
 
 export class ProjectDetail extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.jumpVisualizeProject = () => {
-            if(this.props.project)this.props.visualizeProject(this.props.projectId);
-        };
-    }
+    this.jumpVisualizeProject = () => {
+      if(this.props.project)this.props.visualizeProject(this.props.projectId);
+    };
+  }
 
-    render() {
-        if(!this.props.project) return <div />;
-        const project = sanitizeProject(this.props.project);
-        const isPrivate = project.private;
-        const tags = project.tags.tags;
-        const isTag = tags.length > 0 ? true : false;
-        return (
-            <div>
-                {project ? (
-                    <StyledDetailFrame>
-                        <ProjectTitle lang="ja">
-                            {project.name} {isPrivate && <PrivateNotation>Private Project</PrivateNotation>}
-                        </ProjectTitle>
-                        <TagFrame>
-                            <TagHeader>Tag: </TagHeader>
-                            <TagField>
-                                {isTag ? (
-                                    tags.map((tag, index) => {
-                                        return <StyledTagName key={index}>{tag.name}</StyledTagName>;
-                                    })
-                                ) : (
-                                    <TagHeader>none</TagHeader>
-                                )}
-                            </TagField>
-                        </TagFrame>
-                        <Player />
-                        <ContentsFrame>
-                            <DescriptionFrame>
-                                <StyledHead>Description</StyledHead>
-                                <StyledDescription>{project.description}</StyledDescription>
-                            </DescriptionFrame>
-                            <StatusFrame>
-                                <StyledHead>Author</StyledHead>
-                                <StatusText>{project.user.nickname}</StatusText>
-                                <StyledHead>Created Date</StyledHead>
-                                <StatusText>{project.date}</StatusText>
-                            </StatusFrame>
-                        </ContentsFrame>
-                        <CaptionList
-                            figures={project.content
-                                .map(content => content.figure)
-                                .sort((fig1, fig2) => fig1.position - fig2.position)}
-                            contentType={this.props.contentType}
-                        />
-                        <div onClick={this.jumpVisualizeProject}>Jump !!</div>
-                        {this.props.showDeleteConfirmation ? <DeleteModal /> : <span />}
-                    </StyledDetailFrame>
+  render() {
+    if(!this.props.project) return <div />;
+    const project = sanitizeProject(this.props.project);
+    const isPrivate = project.private;
+    const tags = project.tags.tags;
+    const isTag = tags.length > 0 ? true : false;
+    return (
+      <div>
+        {project ? (
+          <StyledDetailFrame>
+            <ProjectTitle lang="ja">
+              {project.name} {isPrivate && <PrivateNotation>Private Project</PrivateNotation>}
+            </ProjectTitle>
+            <TagFrame>
+              <TagHeader>Tag: </TagHeader>
+              <TagField>
+                {isTag ? (
+                  tags.map((tag, index) => {
+                    return <StyledTagName key={index}>{tag.name}</StyledTagName>;
+                  })
                 ) : (
-                    <div> loading project... </div>
+                  <TagHeader>none</TagHeader>
                 )}
-            </div>
-        );
-    }
+              </TagField>
+            </TagFrame>
+            <Player />
+            <ContentsFrame>
+              <DescriptionFrame>
+                <StyledHead>Description</StyledHead>
+                <StyledDescription>{project.description}</StyledDescription>
+              </DescriptionFrame>
+              <StatusFrame>
+                <StyledHead>Author</StyledHead>
+                <StatusText>{project.user.nickname}</StatusText>
+                <StyledHead>Created Date</StyledHead>
+                <StatusText>{project.date}</StatusText>
+              </StatusFrame>
+            </ContentsFrame>
+            <CaptionList
+              figures={project.content
+                .map(content => content.figure)
+                .sort((fig1, fig2) => fig1.position - fig2.position)}
+              contentType={this.props.contentType}
+            />
+            <div onClick={this.jumpVisualizeProject}>Jump !!</div>
+            {this.props.showDeleteConfirmation ? <DeleteModal /> : <span />}
+          </StyledDetailFrame>
+        ) : (
+          <div> loading project... </div>
+        )}
+      </div>
+    );
+  }
 }
 
 ProjectDetail.propTypes = {
-    project: PropTypes.object,
-    userId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    userIsAdmin: PropTypes.bool,
-    showDeleteConfirmation: PropTypes.bool,
-    targetProject: PropTypes.number,
-    contentType: PropTypes.string,
-    visualizeProject: PropTypes.func
+  project: PropTypes.object,
+  userId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  userIsAdmin: PropTypes.bool,
+  showDeleteConfirmation: PropTypes.bool,
+  targetProject: PropTypes.number,
+  contentType: PropTypes.string,
+  visualizeProject: PropTypes.func
 };
 
 const mapStateToProps = state => ({
-    project: state.manager.targetProject,
-    userId: state.user.id,
-    userIsAdmin: state.user.isAdmin,
-    showDeleteConfirmation: state.modals.showDeleteConfirmation,
-    targetProject: state.modals.targetProject,
-    contentType: state.player.contentType
+  project: state.manager.targetProject,
+  userId: state.user.id,
+  userIsAdmin: state.user.isAdmin,
+  showDeleteConfirmation: state.modals.showDeleteConfirmation,
+  targetProject: state.modals.targetProject,
+  contentType: state.player.contentType
 });
 
 const mapDispatchToProps = dispatch => ({
-    visualizeProject: projectId => {
-        dispatch(push(`/visualizer/${projectId}`));
-    }
+  visualizeProject: projectId => {
+    dispatch(push(`/visualizer/${projectId}`));
+  }
 });
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(ProjectDetail);
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -15,7 +15,7 @@ import ProjectPlayer from './components/ProjectPlayer';
 import CreateProject from './components/CreateProject';
 import ProjectEditForm from './components/ProjectEditForm';
 import ProjectDetail from './components/ProjectDetail';
-import Visualizer from './components/Visualizer.js';
+import Visualizer from './components/Visualizer';
 import WorkSpace from './components/WorkSpace';
 
 import reducers from './reducers/index';
@@ -36,75 +36,75 @@ import { host } from './utils/host';
 const debug = Debug('fabnavi:jsx:FabnaviApp');
 
 const forceSignIn = store => {
-    debug('force login');
-    const authUrl = `${host.url}/auth/github?auth_origin_url=${host.url}`;
-    const authWindow = new remote.BrowserWindow({
-        modal: true,
-        width: 400,
-        height: 800,
-        webPreferences: {
-            webSecurity: false
-        }
-    });
-    authWindow.loadURL(authUrl);
-    const onMessage = () => {
-        debug(authWindow.getURL());
-        const url = authWindow.getURL();
-        if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')) {
-            const credential = {
-                accessToken: url.match(/auth_token=([a-zA-Z0-9\-_]*)/)[1],
-                uid: url.match(/uid=([a-zA-Z0-9\-_]*)/)[1],
-                client: url.match(/client_id=([a-zA-Z0-9\-_]*)/)[1]
-            };
-            api.saveCredential(credential);
-            store.dispatch(signedIn(credential));
-            authWindow.close();
-        }
-    };
-    authWindow.once('message', onMessage);
-    authWindow.on('page-title-updated', onMessage);
+  debug('force login');
+  const authUrl = `${host.url}/auth/github?auth_origin_url=${host.url}`;
+  const authWindow = new remote.BrowserWindow({
+    modal: true,
+    width: 400,
+    height: 800,
+    webPreferences: {
+      webSecurity: false
+    }
+  });
+  authWindow.loadURL(authUrl);
+  const onMessage = () => {
+    debug(authWindow.getURL());
+    const url = authWindow.getURL();
+    if(url.includes('uid') && url.includes('client_id') && url.includes('auth_token')) {
+      const credential = {
+        accessToken: url.match(/auth_token=([a-zA-Z0-9\-_]*)/)[1],
+        uid: url.match(/uid=([a-zA-Z0-9\-_]*)/)[1],
+        client: url.match(/client_id=([a-zA-Z0-9\-_]*)/)[1]
+      };
+      api.saveCredential(credential);
+      store.dispatch(signedIn(credential));
+      authWindow.close();
+    }
+  };
+  authWindow.once('message', onMessage);
+  authWindow.on('page-title-updated', onMessage);
 };
 if(isDev) {
-    window.api = WebAPIUtils;
+  window.api = WebAPIUtils;
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    debug('======> Mount App');
-    const history = createMemoryHistory();
-    const composeEnhancers = isDev ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
-    const store = createStore(
-        reducers,
-        composeEnhancers(applyMiddleware(adjustor, epicsMiddleware, routerMiddleware(history)))
-    );
-    api.init(store);
-    debug(api.loadCredential());
-    if(!api.loadCredential()) forceSignIn(store);
-    store.dispatch(fetchProjects(0, 'all'));
-    ReactDOM.render(
-        <Provider store={store}>
-            <ConnectedRouter history={history}>
+  debug('======> Mount App');
+  const history = createMemoryHistory();
+  const composeEnhancers = isDev ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
+  const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(adjustor, epicsMiddleware, routerMiddleware(history)))
+  );
+  api.init(store);
+  debug(api.loadCredential());
+  if(!api.loadCredential()) forceSignIn(store);
+  store.dispatch(fetchProjects(0, 'all'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Switch>
+          <Route component={ProjectPlayer} path="/play/:projectId" />
+          <Route component={WorkSpace} path="/workspace" />
+          <Route component={Visualizer} path="/visualizer/:projectId" />
+          <Route
+            path="/"
+            render={() => (
+              <ProjectManager>
                 <Switch>
-                    <Route component={ProjectPlayer} path="/play/:projectId" />
-                    <Route component={WorkSpace} path="/workspace" />
-                    <Route component={Visualizer} path="/visualizer/:projectId" />
-                    <Route
-                        path="/"
-                        render={() => (
-                            <ProjectManager>
-                                <Switch>
-                                    <Route component={ProjectList} path="/" exact />
-                                    <Route component={ProjectList} path="/myprojects" />
-                                    <Route component={CreateProject} path="/create" />
-                                    <Route component={ProjectEditForm} path="/edit/:projectId" />
-                                    <Route component={ProjectDetail} path="/detail/:projectId" />
-                                </Switch>
-                            </ProjectManager>
-                        )}
-                    />
+                  <Route component={ProjectList} path="/" exact />
+                  <Route component={ProjectList} path="/myprojects" />
+                  <Route component={CreateProject} path="/create" />
+                  <Route component={ProjectEditForm} path="/edit/:projectId" />
+                  <Route component={ProjectDetail} path="/detail/:projectId" />
                 </Switch>
-            </ConnectedRouter>
-        </Provider>,
-        document.getElementById('app')
-    );
-    window.addEventListener('keydown', handleKeyDown(store));
+              </ProjectManager>
+            )}
+          />
+        </Switch>
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('app')
+  );
+  window.addEventListener('keydown', handleKeyDown(store));
 });
