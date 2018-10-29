8b8b14e8c805d7cc67efc6b5f57e55d867320a76
fmsuvM <k222.tkd.fms@gmail.com>
2018-10-29 15:03:23 +0900

    style update

--- src/renderer/stylesheets/visualizer/ProcessVisualizer.js
+++ src/renderer/stylesheets/visualizer/ProcessVisualizer.js
@@ -1,7 +1,9 @@
 import styled, { css } from 'styled-components';
 
 export const VisualizeFrame = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 0;
   display: flex;
   flex-direction: column;
-  margin: 30px 50px 30px 50px;
 `;
